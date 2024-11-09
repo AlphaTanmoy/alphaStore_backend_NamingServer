d0f4d22c5f8b5e23ecaaca40801175bbbd10f33a
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:32:30 +0530

    update

---
+++ src/test/kotlin/com/alphaStore/NamingServer/NamingServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphaStore.NamingServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
